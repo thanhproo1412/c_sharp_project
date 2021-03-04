3840e1a52d9aa54ac39e45ff90f69001d6ec1c40
Hai Thanh <thanhproo1412@gmail.com>
2021-03-04 14:02:29 +0700

    ss
    ss

---
+++ First_project/First_project/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace First_project
+{
+    class danhSachHocSinh
+    {
+        public int SoHS;
+        public string[] dsHS;
+        public int[] dsPoint;
+        
+    }
+    class DanhSachDiem : danhSachHocSinh
+    {
+        static void tinhDiem(danhSachHocSinh a)
+        {
+            Console.WriteLine("Nhom : " + nameof(danhSachHocSinh));
+            Console.WriteLine("So Hoc Sinh Trong Nhom : " + a.SoHS);
+            for (int i = 0; i < a.SoHS; i++)
+            {
+                Console.WriteLine("Hoc sinh " + a.dsHS[i] + ": " + a.dsPoint[i] + " diem");
+            }
+        }
+        static void Main()
+        {
+            danhSachHocSinh NhomA = new danhSachHocSinh();
+            NhomA.SoHS = 4;
+            NhomA.dsHS = new [] { "nguyen", "sy", "hai", "thanh" };
+            NhomA.dsPoint = new [] { 9, 7, 10, 8 };
+            danhSachHocSinh NhomB = new danhSachHocSinh();
+            NhomB.SoHS = 4;
+            NhomB.dsHS = new[] { "thanh", "nguyen", "sy", "hai" };
+            NhomB.dsPoint = new[] { 10, 7, 8, 9 };
+            tinhDiem(NhomA);
+            tinhDiem(NhomB);
+        }
+    }
+}
---
+++ nhap_diem/nhap_diem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace nhap_diem
+{
+    class XinChao
+    {
+        static void Main()
+        {
+            Console.WriteLine("so hoc sinh : ");
+            int soHS = Convert.ToInt32(Console.ReadLine());
+            string[] dsHS = new string[soHS];
+            int[] dsPoint = new int[soHS];
+            for (int i = 0; i < soHS; i++)
+            {
+                Console.WriteLine("Ten Hoc sinh so " + (i + 1));
+                dsHS[i] = Console.ReadLine();
+                Console.WriteLine("Diem hoc sinh so " + (i + 1));
+                dsPoint[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Xin chao cac ban");
+            for (int i = 0; i < soHS; i++)
+            {
+                Console.WriteLine("Hoc sinh " + dsHS[i] + ": " + dsPoint[i] + " diem");
+            }
+        }
+    }
+}
